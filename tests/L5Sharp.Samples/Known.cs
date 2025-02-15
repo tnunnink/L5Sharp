55365fc40e45dd253696aceaf88c0c8c995d209e
tnunnink <timothyjnunnink@gmail.com>
2025-02-14 19:56:05 -0600

    Added ACD test file

--- tests/L5Sharp.Samples/Known.cs
+++ tests/L5Sharp.Samples/Known.cs
@@ -8,6 +8,7 @@ namespace L5Sharp.Samples
         public static readonly string Directory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
 
         public static readonly string Test = Path.Combine(Directory, "Test.xml");
+        public static readonly string TestAcd = Path.Combine(Directory, "Test.ACD");
         public static readonly string Empty = Path.Combine(Directory, "Empty.xml");
         public static readonly string LotOfTags = Path.Combine(Directory, "LotOfTags.xml");
         public const string Example = @"C:\Users\tnunn\Documents\L5X\Example.L5X";
