235b03bb9a3f23c19af33ed9f19bd7f4a2766d2f
tnunnink <timothyjnunnink@gmail.com>
2026-03-20 10:54:25 -0500

    Removed unused constants and methods from `Known` test sample, simplifying the class structure.

--- tests/L5Sharp.Tests.Samples/Known.cs
+++ tests/L5Sharp.Tests.Samples/Known.cs
@@ -1,14 +1,7 @@
-﻿using System.IO;
-using System.Runtime.CompilerServices;
-
-namespace L5Sharp.Tests.Samples;
+﻿namespace L5Sharp.Tests.Samples;
 
 public static class Known
 {
-    private static string GetThisFilePath([CallerFilePath] string? path = null) => path ?? string.Empty;
-    public static readonly string Directory = Path.GetDirectoryName(GetThisFilePath())!;
-    public static readonly string ModuleExport = Path.Combine(Directory, "Modules", "TestCard.L5X");
-
     public const string DataType = "SimpleType";
     public const string AddOnInstruction = "aoi_Test";
     public const string Module = "TestCard";
