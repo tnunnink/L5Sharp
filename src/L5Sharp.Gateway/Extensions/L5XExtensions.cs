2198d65ed5cd7ca56c52d1439f6b7bf520b3ebf2
tnunnink <timothyjnunnink@gmail.com>
2026-04-24 09:15:15 -0500

    Made `UploadAsync` methods in `L5XExtensions` static for enhanced usability and updated XML documentation for clarity. Incremented version to 1.1.1.

--- src/L5Sharp.Gateway/Extensions/L5XExtensions.cs
+++ src/L5Sharp.Gateway/Extensions/L5XExtensions.cs
@@ -5,73 +5,67 @@ using System.Threading.Tasks;
 using L5Sharp.Core;
 using L5Sharp.Gateway.Abstractions;
 using L5Sharp.Gateway.Common;
+// ReSharper disable ConvertToExtensionBlock
 
 namespace L5Sharp.Gateway.Extensions;
 
 /// <summary>
-/// Provides extension methods for working with the L5Sharp framework, enabling convenient operations
-/// such as uploading and managing tags in PLC projects using the provided client.
+/// Provides extension methods for <see cref="L5X"/> instances that enable uploading tag values
+/// from a PLC controller to update the L5X project data.
 /// </summary>
 public static class L5XExtensions
 {
     /// <summary>
-    /// Provides extension methods for interacting with L5X project instances, enabling functionality
-    /// such as uploading tags from the project to a connected PLC controller.
+    /// Reads current values of all public tags from the PLC controller and returns the results.
     /// </summary>
-    extension(L5X project)
+    /// <param name="project">The L5X project containing the tags to upload.</param>
+    /// <param name="client">The PLC client used to communicate with the controller.</param>
+    /// <param name="token">A cancellation token to cancel the upload operation.</param>
+    /// <returns>A task that represents the asynchronous upload operation containing a <see cref="TagResults"/> with the results.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="client"/> is null.</exception>
+    public static Task<TagResults> UploadAsync(this L5X project, IPlcClient client, CancellationToken token = default)
     {
-        /// <summary>
-        /// Uploads all public tags from the L5X project to the PLC by reading their
-        /// current values from the controller.
-        /// </summary>
-        /// <param name="client">The PLC client used to communicate with the controller.</param>
-        /// <param name="token">A cancellation token to cancel the upload operation.</param>
-        /// <returns>A task that represents the asynchronous upload operation containing a <see cref="TagResults"/> with the results.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="client"/> is null.</exception>
-        public Task<TagResults> UploadAsync(IPlcClient client, CancellationToken token = default)
-        {
-            if (client is null) throw new ArgumentNullException(nameof(client));
+        if (client is null) throw new ArgumentNullException(nameof(client));
 
-            var tags = project.Query<Tag>().Where(t => t.IsPublic()).ToArray();
-            var response = client.ReadTags(tags, token);
-            return response;
-        }
+        var tags = project.Query<Tag>().Where(t => t.IsPublic()).ToArray();
+        var response = client.ReadTags(tags, token);
+        return response;
+    }
 
-        /// <summary>
-        /// Uploads public tags from the L5X project that match the specified predicate by reading their
-        /// current values from the controller.
-        /// </summary>
-        /// <param name="client">The PLC client used to communicate with the controller.</param>
-        /// <param name="predicate">A function to filter which tags should be uploaded.</param>
-        /// <param name="token">A cancellation token to cancel the upload operation.</param>
-        /// <returns>A task that represents the asynchronous upload operation containing a <see cref="TagResults"/> with the results.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="client"/> or <paramref name="predicate"/> is null.</exception>
-        public Task<TagResults> UploadAsync(IPlcClient client, Func<Tag, bool> predicate, CancellationToken token = default)
-        {
-            if (client is null) throw new ArgumentNullException(nameof(client));
-            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+    /// <summary>
+    /// Reads current values of public tags that match the specified predicate from the PLC controller and returns the results.
+    /// </summary>
+    /// <param name="project">The L5X project containing the tags to upload.</param>
+    /// <param name="client">The PLC client used to communicate with the controller.</param>
+    /// <param name="predicate">A function to filter which tags should be uploaded.</param>
+    /// <param name="token">A cancellation token to cancel the upload operation.</param>
+    /// <returns>A task that represents the asynchronous upload operation containing a <see cref="TagResults"/> with the results.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="client"/> or <paramref name="predicate"/> is null.</exception>
+    public static Task<TagResults> UploadAsync(this L5X project, IPlcClient client, Func<Tag, bool> predicate, CancellationToken token = default)
+    {
+        if (client is null) throw new ArgumentNullException(nameof(client));
+        if (predicate is null) throw new ArgumentNullException(nameof(predicate));
 
-            var tags = project.Query(predicate).Where(t => t.IsPublic()).ToArray();
-            var response = client.ReadTags(tags, token);
-            return response;
-        }
+        var tags = project.Query(predicate).Where(t => t.IsPublic()).ToArray();
+        var response = client.ReadTags(tags, token);
+        return response;
+    }
 
-        /// <summary>
-        /// Uploads all public tags from the L5X project within the specified scope by reading their
-        /// current values from the controller.
-        /// </summary>
-        /// <param name="client">The PLC client used to communicate with the controller.</param>
-        /// <param name="scope">The scope that filters which tags should be uploaded (e.g., controller or program scope).</param>
-        /// <param name="token">A cancellation token to cancel the upload operation.</param>
-        /// <returns>A task that represents the asynchronous upload operation containing a <see cref="TagResults"/> with the results.</returns>
-        /// <exception cref="ArgumentNullException">Thrown when <paramref name="client"/> is null.</exception>
-        public Task<TagResults> UploadAsync(IPlcClient client, Scope scope, CancellationToken token = default)
-        {
-            if (client is null) throw new ArgumentNullException(nameof(client));
+    /// <summary>
+    /// Reads current values of all public tags within the specified scope from the PLC controller and returns the results.
+    /// </summary>
+    /// <param name="project">The L5X project containing the tags to upload.</param>
+    /// <param name="client">The PLC client used to communicate with the controller.</param>
+    /// <param name="scope">The scope that filters which tags should be uploaded (e.g., controller or program scope).</param>
+    /// <param name="token">A cancellation token to cancel the upload operation.</param>
+    /// <returns>A task that represents the asynchronous upload operation containing a <see cref="TagResults"/> with the results.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="client"/> is null.</exception>
+    public static Task<TagResults> UploadAsync(this L5X project, IPlcClient client, Scope scope, CancellationToken token = default)
+    {
+        if (client is null) throw new ArgumentNullException(nameof(client));
 
-            var tags = project.Query<Tag>().Where(t => t.Scope == scope && t.IsPublic()).ToArray();
-            var response = client.ReadTags(tags, token);
-            return response;
-        }
+        var tags = project.Query<Tag>().Where(t => t.Scope == scope && t.IsPublic()).ToArray();
+        var response = client.ReadTags(tags, token);
+        return response;
     }
 }
